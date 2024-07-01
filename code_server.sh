5c413fca0e4a15528b529f026b2bea78e26cd235
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-07-02 02:58:51 +0700

    update to use password

--- code_server.sh
+++ code_server.sh
@@ -1,2 +1,2 @@
 PASSWORD=$(openssl rand -base64 20)
-PASSWORD=$PASSWORD code-server --bind-addr 0.0.0.0:$PORT --disable-telemetry
+PASSWORD=$PASSWORD code-server --bind-addr 0.0.0.0:$PORT --auth password --disable-telemetry
