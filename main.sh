7377e347aa8e341c8796f04bd0d8dc24c9619312
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-11-29 00:38:27 +0700

    solve url

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-echo <<uuid_job_id>> > job.url
+echo <<uuid_job_id>> > <<river_home>>/.river/jobs/<<uuid_job_id>>/job.url
 openvscode-server --host 0.0.0.0 --port $PORT --server-base-path <<uuid_job_id>> --without-connection-token
