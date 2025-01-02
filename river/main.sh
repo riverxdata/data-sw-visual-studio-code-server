4db95fde81fc193b3acd16c47bbbf35dc188cb91
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-01-02 23:49:15 +0700

    append /

--- river/main.sh
+++ river/main.sh
@@ -1,1 +1,1 @@
-openvscode-server --host 0.0.0.0 --port $PORT --server-base-path <<uuid_job_id>> --without-connection-token
+openvscode-server --host 0.0.0.0 --port $PORT --server-base-path /<<uuid_job_id>> --without-connection-token
