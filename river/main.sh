535727d14578a79b0dee71b367f6d1020fd250c4
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-02-16 11:28:59 +0700

    update to download openvscode automatically, pre-release

--- river/main.sh
+++ river/main.sh
@@ -7,4 +7,17 @@ echo $PORT > $RIVER_HOME/.river/jobs/$uuid_job_id/job.port
 echo $(hostname) > $RIVER_HOME/.river/jobs/$uuid_job_id/job.host
 echo "$uuid_job_id" > $RIVER_HOME/.river/jobs/$uuid_job_id/job.url
 
+version=1.96.4
+TARGET_DIR="$RIVER_HOME/.river/tools/openvscode-server-v${version}-linux-x64"
+ARCHIVE_URL=https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v${version}/openvscode-server-v${version}-linux-x64.tar.gz
+mkdir -p "$RIVER_HOME/.river/tools"
+# Download & extract only if not already installed
+if [ ! -d "$TARGET_DIR" ]; then 
+    wget -qO "/tmp/openvscode.tar.gz" "$ARCHIVE_URL"
+    tar -xzvf "/tmp/openvscode.tar.gz" -C "$RIVER_HOME/.river/tools/"
+    rm "/tmp/openvscode.tar.gz"
+else
+    echo "OpenVSCode Server already installed at $TARGET_DIR"
+fi
+export PATH=$TARGET_DIR/bin:$PATH
 openvscode-server --host 0.0.0.0 --port $PORT --server-base-path $uuid_job_id --without-connection-token
