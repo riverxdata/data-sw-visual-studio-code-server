ad9d47b6136c4c25e21404a64b636fb0aceb10c8
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-08-27 23:47:22 +0700

    fix: fix download

--- river/main.sh
+++ river/main.sh
@@ -1,21 +1,18 @@
-while IFS== read key value; do
-    printf -v "$key" "$value"
-done < <(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' config.json)
-
 PORT=$(python3 -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
 echo $PORT > $RIVER_HOME/jobs/$job_id/job.port
 echo $(hostname) > $RIVER_HOME/jobs/$job_id/job.host
 echo "$job_id" > $RIVER_HOME/jobs/$job_id/job.url
 TARGET_DIR="$RIVER_HOME/tools/openvscode-server-linux-x64"
-wget $(curl -s https://api.github.com/repos/gitpod-io/openvscode-server/releases/latest \
+mkdir -p "$TARGET_DIR"
+ARCHIVE_URL=$(curl -s https://api.github.com/repos/gitpod-io/openvscode-server/releases/latest \
   | grep "browser_download_url.*linux-x64.tar.gz" \
   | cut -d '"' -f 4)
 
 mkdir -p "$RIVER_HOME/tools"
 # Download & extract only if not already installed
-if [ ! -d "$TARGET_DIR" ]; then 
+if [ ! -f "$TARGET_DIR/bin/openvscode-server" ]; then 
     wget -qO "/tmp/openvscode.tar.gz" "$ARCHIVE_URL"
-    tar -xzvf "/tmp/openvscode.tar.gz" -C "$RIVER_HOME/tools/"
+    tar -xzvf "/tmp/openvscode.tar.gz" -C "$TARGET_DIR" --strip-components=1
     rm "/tmp/openvscode.tar.gz"
 else
     echo "OpenVSCode Server already installed at $TARGET_DIR"
